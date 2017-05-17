4c8cd4e4a0777bbe32b26b12ec41d2909bdea21a
Dustin <pawlowicz.dustin@gmail.com>
2017-05-17 17:47:42 +0200

    CatanBaseCommit

---
+++ src/com/company/Feld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Dustin on 17.05.2017.
+ */
+public class Feld {
+    private static int id = 0;
+    private String name;
+
+    public Feld() {
+    }
+}
